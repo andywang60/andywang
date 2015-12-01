6f41d18306c4fb8befdbd5bf7498ec2f746ad9f2
andy <andy@qq.com>
2015-12-01 10:20:59 +0800

    andywang-lacal

---
+++ andywang-local/andywang-local/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  andywang-local
+//
+//  Created by 408127 on 15/12/1.
+//  Copyright © 2015年 WZF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ andywang-local/andywang-local/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  andywang-local
+//
+//  Created by 408127 on 15/12/1.
+//  Copyright © 2015年 WZF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
